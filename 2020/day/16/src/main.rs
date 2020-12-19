2e2fc34ae5833509245daecd0a1397381262fc7c
Andreas Mårtensson <andreas@addem.se>
2020-12-19 17:24:25 +0100

    2020/day/16 - refactor

--- 2020/day/16/src/main.rs
+++ 2020/day/16/src/main.rs
@@ -1,4 +1,6 @@
 #![feature(str_split_once)]
+#![feature(iterator_fold_self)]
+use std::collections::HashSet;
 use std::io;
 use std::io::Read;
 
@@ -6,9 +8,141 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    let (fields, tickets, others) = parse(&input);
-    println!("{}", scan_others(&fields, &others));
-    println!("{:?}", get_mapping(&fields, &tickets, &others));
+    let (fields, ticket, tickets) = parse(&input);
+    println!("{}", scan_tickets(&fields, &tickets));
+    let order = get_order(&fields, &tickets).unwrap();
+    let res = ticket
+        .iter()
+        .zip(order.iter())
+        .filter(|(_, f)| f.starts_with("departure"))
+        .fold(1, |acc, (v, _)| acc * v);
+    println!("{:?}", res);
+}
+
+fn scan_tickets(fields: &Vec<(&str, Vec<(usize, usize)>)>, tickets: &Vec<Vec<usize>>) -> usize {
+    let mut res = 0;
+    for ticket in tickets.iter() {
+        for value in ticket.iter() {
+            let any = fields.iter().any(|(_, ranges)| {
+                ranges
+                    .iter()
+                    .any(|(low, high)| value >= low && value <= high)
+            });
+            if !any {
+                res += value;
+            }
+        }
+    }
+    return res;
+}
+
+fn get_order<'a>(
+    fields: &'a Vec<(&str, Vec<(usize, usize)>)>,
+    tickets: &Vec<Vec<usize>>,
+) -> Option<Vec<&'a str>> {
+    let valid = tickets
+        .iter()
+        .filter_map(|ticket| {
+            let sets = ticket
+                .iter()
+                .map(|value| {
+                    fields
+                        .iter()
+                        .filter(|(_, ranges)| {
+                            ranges
+                                .iter()
+                                .any(|(low, high)| value >= low && value <= high)
+                        })
+                        .map(|(field, _)| *field)
+                        .collect::<HashSet<_>>()
+                })
+                .collect::<Vec<_>>();
+            if sets.iter().any(|s| s.is_empty()) {
+                return None;
+            }
+            return Some(sets);
+        })
+        .collect::<Vec<_>>();
+    let options = fields
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            valid
+                .iter()
+                .map(|v| v.get(i).unwrap().to_owned())
+                .fold_first(|acc, item| acc.intersection(&item).map(|x| *x).collect())
+                .unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    return pick(options);
+}
+
+fn pick(options: Vec<HashSet<&str>>) -> Option<Vec<&str>> {
+    if let Some((head, tail)) = options.split_first() {
+        for option in head.iter() {
+            let rest = tail
+                .iter()
+                .map(|s| {
+                    let mut new = s.clone();
+                    new.remove(option);
+                    return new;
+                })
+                .collect();
+            if let Some(mut x) = pick(rest) {
+                let mut vec = vec![*option];
+                vec.append(x.as_mut());
+                return Some(vec);
+            }
+        }
+        return None;
+    } else {
+        return Some([].to_vec());
+    }
+}
+
+#[test]
+fn test_example() {
+    let input = "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+"
+    .to_string();
+
+    let (fields, _ticket, tickets) = parse(&input);
+    assert_eq!(scan_tickets(&fields, &tickets), 71);
+}
+
+#[test]
+fn test_example2() {
+    let input = "class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"
+    .to_string();
+
+    let (fields, _ticket, tickets) = parse(&input);
+    assert_eq!(
+        get_order(&fields, &tickets),
+        Some(vec!["row", "class", "seat"])
+    );
 }
 
 fn parse<'a>(
@@ -45,7 +179,7 @@ fn parse<'a>(
         .split(",")
         .map(|x| x.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
-    let others = sections
+    let tickets = sections
         .next()
         .unwrap()
         .strip_prefix("nearby tickets:\n")
@@ -59,87 +193,5 @@ fn parse<'a>(
         })
         .collect::<Vec<_>>();
 
-    return (fields, ticket, others);
-}
-
-fn scan_others(fields: &Vec<(&str, Vec<(usize, usize)>)>, others: &Vec<Vec<usize>>) -> usize {
-    let mut res = 0;
-    for ticket in others.iter() {
-        for value in ticket.iter() {
-            let any = fields.iter().any(|(_, ranges)| {
-                ranges
-                    .iter()
-                    .any(|(low, high)| value >= low && value <= high)
-            });
-            if !any {
-                res += value;
-            }
-        }
-    }
-    return res;
-}
-
-fn get_mapping<'a>(
-    fields: &'a Vec<(&str, Vec<(usize, usize)>)>,
-    ticket: &Vec<usize>,
-    others: &Vec<Vec<usize>>,
-) -> Vec<&'a str> {
-    let candidates = [("row", 0), ("seat", 0)];
-    let valid = others
-        .iter()
-        .filter(|other| {
-            other.iter().all(|value| {
-                fields.iter().any(|(_, ranges)| {
-                    ranges
-                        .iter()
-                        .any(|(low, high)| value >= low && value <= high)
-                })
-            })
-        })
-        .collect::<Vec<_>>();
-
-    println!("{:?}", valid);
-    println!("{:?}", ticket);
-    return vec![""];
-}
-
-#[test]
-fn test_example() {
-    let input = "class: 1-3 or 5-7
-row: 6-11 or 33-44
-seat: 13-40 or 45-50
-
-your ticket:
-7,1,14
-
-nearby tickets:
-7,3,47
-40,4,50
-55,2,20
-38,6,12
-"
-    .to_string();
-
-    let (fields, _tickets, others) = parse(&input);
-    assert_eq!(scan_others(&fields, &others), 71);
-}
-
-#[test]
-fn test_example2() {
-    let input = "class: 0-1 or 4-19
-row: 0-5 or 8-19
-seat: 0-13 or 16-19
-
-your ticket:
-11,12,13
-
-nearby tickets:
-3,9,18
-15,1,5
-5,14,9
-"
-    .to_string();
-
-    let (fields, tickets, others) = parse(&input);
-    println!("{:?}", get_mapping(&fields, &tickets, &others));
+    return (fields, ticket, tickets);
 }
