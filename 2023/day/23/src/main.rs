3a69e688cb080671e04d0742a457008331b0077c
Andreas Mårtensson <andreas@addem.se>
2023-12-23 13:58:12 +0100

    d23p2 very slow

--- 2023/day/23/src/main.rs
+++ 2023/day/23/src/main.rs
@@ -10,7 +10,7 @@ fn do_it(input: &str) -> usize {
     let (nodes, start, end) = parse(input);
 
     let mut q = VecDeque::from([vec![start]]);
-    let mut finished= vec![];
+    let mut finished = vec![];
     while let Some(path) = q.pop_front() {
         for edge in nodes.get(path.last().unwrap()).unwrap() {
             if edge == &end {
@@ -33,9 +33,29 @@ fn do_it(input: &str) -> usize {
 }
 
 fn do_it_again(input: &str) -> usize {
-    let (nodes, start, end) = parse(input);
+    let (nodes, start, end) = parse_again(input);
 
-    0
+    let mut q = VecDeque::from([vec![start]]);
+    let mut finished = vec![];
+    while let Some(path) = q.pop_front() {
+        for edge in nodes.get(path.last().unwrap()).unwrap() {
+            if edge == &end {
+                finished.push(path.clone());
+            }
+
+            if !path.contains(edge) {
+                let mut new_path = path.clone();
+                new_path.push(*edge);
+                q.push_back(new_path);
+            }
+        }
+    }
+
+    // for f in finished.iter() {
+    //     println!("{}: {f:?}", f.len());
+    // }
+
+    finished.iter().map(|p| p.len()).max().unwrap()
 }
 
 fn parse(
@@ -50,13 +70,51 @@ fn parse(
         .enumerate()
         .flat_map(|(y, l)| {
             l.chars().enumerate().filter_map(move |(x, c)| match c {
-                '.' if y > 0=> Some(((x, y), vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])),
+                '.' if y > 0 => {
+                    Some(((x, y), vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]))
+                }
                 '.' => Some(((x, y), vec![(x - 1, y), (x + 1, y), (x, y + 1)])),
                 '>' => Some(((x, y), vec![(x + 1, y)])),
                 '<' => Some(((x, y), vec![(x - 1, y)])),
                 '^' => Some(((x, y), vec![(x, y - 1)])),
                 'v' => Some(((x, y), vec![(x, y + 1)])),
                 _ => None,
+            })
+        })
+        .collect();
+
+    let exists: HashSet<_> = all.iter().map(|&(p, _)| p).collect();
+
+    let mut nodes = HashMap::new();
+    for ((x, y), edges) in all.iter() {
+        nodes.insert(
+            (*x, *y),
+            edges
+                .clone()
+                .into_iter()
+                .filter(|c| exists.contains(c))
+                .collect(),
+        );
+    }
+
+    (nodes, all.first().unwrap().0, all.last().unwrap().0)
+}
+
+fn parse_again(
+    input: &str,
+) -> (
+    HashMap<(usize, usize), Vec<(usize, usize)>>,
+    (usize, usize),
+    (usize, usize),
+) {
+    let all: Vec<_> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            l.chars().enumerate().filter_map(move |(x, c)| match c {
+                '#' => None,
+                _ if y > 0 => Some(((x, y), vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])),
+                _ => Some(((x, y), vec![(x - 1, y), (x + 1, y), (x, y + 1)])),
             })
         })
         .collect();
@@ -108,6 +166,5 @@ fn test() {
     .replace("        ", "");
 
     assert_eq!(do_it(input.as_str()), 94);
-
-    //assert_eq!(do_it_again(input.as_str()), 0);
+    assert_eq!(do_it_again(input.as_str()), 154);
 }
